8232185ffa6f0d4fde6b1b47ecf8a8779fdf5bf2
Sudhir Panda <sudhirpanda@outlook.com>
2015-12-18 11:33:47 +0530

    First Change

---
+++ test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Hello Ashutosh
+
+            //       SELECT*
+            //       FROM   Advertisement INNER JOIN
+            //       SubCategory ON Advertisement.SubCategoryID = SubCategory.SubCategoryID INNER JOIN
+            //       CategoryHaveSubCategory ON SubCategory.SubCategoryID = CategoryHaveSubCategory.SubCategoryID INNER JOIN
+            //       Category ON CategoryHaveSubCategory.CategoryID = Category.CategoryID
+            //       WHERE(CategoryHaveSubCategory.CategoryID = 1)
+ 
+        }
+    }
+}
